c8a9854c423afe76e47ed03b42659803031897a9
Deni2 <zartzurt2_@hotmail.com>
2025-09-29 00:34:25 +0300

    balance

--- workloads/transaction/app.py
+++ workloads/transaction/app.py
@@ -29,4 +29,4 @@ def withdraw():
             return jsonify({"error": "Not enough funds"}), 400
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5050)
