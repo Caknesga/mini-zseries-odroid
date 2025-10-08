70b6bd02d86d0e74caa630ee37b66d050490be1c
Deni2 <zartzurt2_@hotmail.com>
2025-10-08 15:51:07 +0300

    dynamic flask

--- workloads/transaction/app.py
+++ workloads/transaction/app.py
@@ -1,9 +1,24 @@
 from flask import Flask, request, jsonify, render_template
 import threading
+import json, os
+
 
 app = Flask(__name__)
 accounts = {"A": 1000, "B": 2000, "C": 1500}
 lock = threading.Lock()  # Für Konsistenz
+
+def save_state():
+    with open("accounts.json", "w") as f:
+        json.dump(accounts, f)
+
+def load_state():
+    global accounts
+    if os.path.exists("accounts.json"):
+        with open("accounts.json", "r") as f:
+            accounts = json.load(f)
+    else:
+        accounts = {"A": 1000, "B": 2000, "C": 1500}
+
 
 @app.route("/")
 def index():
@@ -22,7 +37,8 @@ def deposit():
     if account not in accounts:
         return jsonify({"error": "Account not found"}), 404
     with lock:
-        accounts[account] += amount            # <-- FIX
+        accounts[account] += amount
+        save_state()          # <-- FIX
     return jsonify({"account": account, "balance": accounts[account]})
 
 @app.route("/withdraw", methods=["POST"])
@@ -33,10 +49,12 @@ def withdraw():
         return jsonify({"error": "Account not found"}), 404
     with lock:
         if accounts[account] >= amount:
-            accounts[account] -= amount
+            save_state()
             return jsonify({"account": account, "balance": accounts[account]})
         else:
+            save_state()
             return jsonify({"error": "Not enough funds"}), 400
 
 if __name__ == "__main__":
+    load_state()
     app.run(host="0.0.0.0", port=5050)
